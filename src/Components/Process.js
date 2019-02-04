67f2ffd8e6dfe781ff9d468a13c72f9ab2aedac8
Maxime Salomon <maxime@croissant.io>
2019-02-04 18:16:02 +0100

    mobile process + type

--- src/Components/Process.js
+++ src/Components/Process.js
@@ -10,10 +10,10 @@ const Process = () => {
           <h3>1 - We uncover your<br></br>company growth levers</h3>
           <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
-        <div className="gl-right"><img src={coworkersright} height="300px" /></div>
+        <div className="gl-right mobile-none"><img src={coworkersright} height="300px" /></div>
       </div>
       <div className="growth-experiments">
-        <div className="ge-left"><img src={coworkersleft} height="300px" /></div>
+        <div className="ge-left mobile-none"><img src={coworkersleft} height="300px" /></div>
         <div className="ge-right">
           <h3>2 - We pre-select proven<br></br>growth experiments</h3>
           <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -24,7 +24,7 @@ const Process = () => {
           <h3>3 - We design, build & analyse<br></br>your growth experiments</h3>
           <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
-        <div className="dba-right"><img src={coworkersright} height="300px" /></div>
+        <div className="dba-right mobile-none"><img src={coworkersright} height="300px" /></div>
       </div>
       </section>
     )
